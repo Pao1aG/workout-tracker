ffcf70892782226252e257f1f8bd8109822da66b
Pao1a <paolaagonzalezm@email.arizona.edu>
2021-07-21 13:41:36 -0700

    deploy

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/morning-coast-60192", { 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/morning-coast-60192", 
+{ 
   useNewUrlParser: true, 
   useUnifiedTopology: true,
   useCreateIndex: true,
